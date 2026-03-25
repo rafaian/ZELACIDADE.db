281480df68df25068a20196e7ef4ec86fd74d5f1
rafaian <rafaelserafimx@gmail.com>
2026-03-25 11:27:10 -0300

    atualização, realizado CRUD- de UPDATE e DELET

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/incidentes', async (req, res) => {
     
     const db = await criarBanco()
 
-    const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`,[id])
+    const incidenteEspecifico = await db.get(`SELECT * FROM incidentes WHERE id = ?`,[id])
     res.json(incidenteEspecifico)
  })
 
@@ -49,3 +49,35 @@ app.get('/incidentes', async (req, res) => {
       res.send(`Incidente novo registrado: ${tipo_problema} registrado na data: ${data_registro} por: ${nome_solicitante}`)
 
  })
+ 
+ //  Rota de atualização = Ela é responsável por editar um incidente já existente no banco.
+ app.put('/incidentes/:id', async ( req,res ) => {
+  //  pega o incidentes que vem pela URL(ex: /incidentes/:)
+   const{ id } = req.params
+
+   const { prioridade, descricao, status_resolucao } = req.body
+
+  // abre conexao com banco de dados.
+   const db = await criarBanco()
+
+    await db.run(`
+      UPDATE incidentes
+      SET prioridade = ?, descricao = ?, status_resolucao = ?
+      WHERE id = ?
+      `, [prioridade, descricao, status_resolucao, id])
+
+      res.send(`O incidente de ID: ${id} foi atualzado com sucesso!!`)
+})
+
+  app.delete('/incidentes/:id', async (req,res) => {
+
+    const {id} = req.params
+    const db = await criarBanco()
+    
+    await db.run(`DELETE FROM incidentes WHERE id = ?`,[id])
+    
+    res.send(`O incidente de ID ${id} foi removido com sucesso!!`)
+ 
+  })
+
+  
